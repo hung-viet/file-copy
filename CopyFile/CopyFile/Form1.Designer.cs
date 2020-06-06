3e66d55379aec37c238d2ede7aafce6d5e1f8ebc
kunkunkun1234 <Nobi.Nobita.1996@gmail.com>
2020-06-06 17:57:12 +0700

    Initial commit

---
+++ CopyFile/CopyFile/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CopyFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(12, 60);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(560, 326);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(486, 8);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(86, 46);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(99, 34);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(211, 20);
+            this.txtTarget.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Target prefix:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source prefix";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(99, 8);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(211, 20);
+            this.txtSource.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(394, 8);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 46);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 389);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(38, 13);
+            this.lbStatus.TabIndex = 7;
+            this.lbStatus.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ CopyFile/CopyFile/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+namespace CopyFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbStatus.Text = "Doing...";
+                if (!IsValidForm())
+                {
+                    lbStatus.Text = "Error";
+                    return;
+                }
+
+                var data = txtURL.Text.Replace("\r\n", ",") ?? "";
+                List<string> result = data.Split(',').Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+                if (result.Count == 0)
+                {
+                    lbStatus.Text = "Error";
+                    return;
+                }
+
+                foreach (var item in result)
+                {
+                    string sourcePath = Path.GetDirectoryName(item) ?? "";
+                    string fileName = Path.GetFileName(item) ?? "";
+                    string targetPath = sourcePath.Replace(txtSource.Text, txtTarget.Text);
+                    if (Directory.Exists(sourcePath) && Directory.Exists(targetPath) && !string.IsNullOrEmpty(sourcePath) && !string.IsNullOrEmpty(fileName))
+                    {
+                        // Use Path class to manipulate file and directory paths.
+                        string sourceFile = Path.Combine(sourcePath, fileName);
+                        string destFile = Path.Combine(targetPath, fileName);
+
+                        // To copy a file to another location and
+                        // overwrite the destination file if it already exists.
+                        File.Copy(sourceFile, destFile, true);
+                        lbStatus.Text = "Done";
+                    }
+                    else
+                    {
+                        lbStatus.Text = "Error";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                lbStatus.Text = "Error";
+            }
+        }
+
+        private bool IsValidForm()
+        {
+            if (string.IsNullOrWhiteSpace(txtTarget.Text))
+            {
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtTarget.Text))
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtSource.Text))
+            {
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtSource.Text))
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(txtURL.Text))
+            {
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtURL.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Clipboard.GetText()))
+                txtURL.Text += ("\r\n" + Clipboard.GetText());
+        }
+    }
+}
